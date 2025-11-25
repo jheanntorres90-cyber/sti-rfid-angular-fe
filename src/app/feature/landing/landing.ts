03aedd09c36872ab31a01bad10642d400af362f0
LAPTOP-JBP2REQA\jhe ann caila torres <jheanntorres90@gmail.com>
2025-11-26 04:26:55 +0800

    Hotfixed-header-by-arvin

--- src/app/feature/landing/landing.ts
+++ src/app/feature/landing/landing.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { HeaderComponent } from "../../../components/header/header";
+import { HeaderComponent } from '../../components/header/header';
+
 
 
 @Component({
