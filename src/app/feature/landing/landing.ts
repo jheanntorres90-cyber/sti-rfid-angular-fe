a3dd03f0f49124f1816a4ec921ac292f16207d2c
LAPTOP-JBP2REQA\jhe ann caila torres <jheanntorres90@gmail.com>
2025-11-26 03:28:46 +0800

    SSPS-63 - Implement common header - changes in UI

--- src/app/feature/landing/landing.ts
+++ src/app/feature/landing/landing.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../components/header";
+import { HeaderComponent } from '../../components/header/header';
 
 @Component({
   selector: 'app-landing',
