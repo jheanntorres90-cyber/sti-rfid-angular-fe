135f3b32bbc5ce88d3ecc8addb4b9c5462cb6b3d
LAPTOP-JBP2REQA\jhe ann caila torres <jheanntorres90@gmail.com>
2025-11-30 12:22:16 +0800

    SSPS-16 - Implement UI for Modal Login - Admin and Student - b

--- src/app/feature/landing/landing.ts
+++ src/app/feature/landing/landing.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core'
 import { HeaderComponent } from '../../components/header/header';
+import { LoginModalComponent } from '../dialogs/admin/admin-login-dialog/login';
 
 
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, LoginModalComponent],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
